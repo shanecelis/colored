5efbafbea310b4b5e0ec244a7008381808fc1971
Shane Celis <shane.celis@gmail.com>
2023-06-08 07:17:09 -0400

    feature: Print the dynamic_colors.rs.

--- examples/dynamic_colors.rs
+++ examples/dynamic_colors.rs
@@ -3,12 +3,12 @@ use colored::*;
 
 fn main() {
     // the easy way
-    "blue string yo".color("blue");
+    println!("{}", "blue string yo".color("blue"));
 
     // this will default to white
-    "white string".color("zorglub");
+    println!("{}", "white string".color("zorglub"));
 
     // the safer way via a Result
     let color_res = "zorglub".parse(); // <- this returns a Result<Color, ()>
-    "red string".color(color_res.unwrap_or(Color::Red));
+    println!("{}", "red string".color(color_res.unwrap_or(Color::Red)));
 }
